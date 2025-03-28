ee37d997303e61dcaf7aaf06dbc0ee49165c4627
andres carranza <georgepipe2005@gmail.com>
2025-03-27 20:10:03 -0500

    Update taller.py

--- taller.py
+++ taller.py
@@ -1,5 +1,31 @@
 vertices = []
 n = int(input("Ingrese el número de vértices: "))
+
 for i in range(n):
-    x, y, z = input(f"Ingrese las coordenadas del vértice {i+1} (x y z): ")
-    vertices.append((float(x), float(y), float(z)))
+    x = float(input(f"ingrese la coordenada x {i+1}: "))
+    y = float(input(f"ingrese la coordenada y {i+1}: "))
+    z = float(input(f"ingrese la coordenada z {i+1}: "))
+    vertices.append((x, y, z))
+
+caras = []
+a = int(input("Ingrese el número de caras: "))
+
+for i in range(a):
+    c1 = int(input(f"ingrese c1 {i+1}: "))
+    c2 = int(input(f"ingrese c2 {i+1}: "))
+    c3 = int(input(f"ingrese c3 {i+1}: "))
+    caras.append((c1, c2, c3))
+
+print("codigo OBJ:")
+
+for v in vertices:
+    b= "v"
+    for c in v:
+        b += f"{c}"
+    print(b)
+
+for c in caras:
+    f = "f "
+    for i in c:
+        f+= f"{i}"
+    print(f)
